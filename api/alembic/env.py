3c608eb8c0f09a5fbdca1daa73094cdebb04c8d8
elias <eliasbenlaroussi@gmail.com>
2025-05-06 15:39:14 +0200

    Feat: create base structure api

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from sqlmodel import SQLModel
-from main import User
+from src.main import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
